c457a6c4fda0c84ea8eb0840f11304f6a0fbce41
Ben Mann <8enmann@gmail.com>
2017-08-25 15:00:04 -0700

    reimplement MLP and CNN in Keras layers

--- baselines/a2c/policies.py
+++ baselines/a2c/policies.py
@@ -1,5 +1,6 @@
 import numpy as np
 import tensorflow as tf
+from keras.layers import Dense, Conv2D, Flatten
 from baselines.a2c.utils import conv, fc, conv_to_fc, batch_to_seq, seq_to_batch, lstm, lnlstm, sample, check_shape
 from baselines.common.distributions import make_pdtype
 import baselines.common.tf_util as U
@@ -89,17 +90,23 @@ class LstmPolicy(object):
         self.value = value
 
 class FcPolicy(object):
+    """Fully connected NN. Expects input to be flat."""
 
     def __init__(self, sess, ob_space, ac_space, nenv, nsteps, nstack, reuse=False):
-        nbatch = nenv*nsteps
-        ob_shape = (nbatch,) + ob_space.shape
+        ob_shape = (None if USE_KERAS else nenv*nsteps,) + ob_space.shape
         nact = ac_space.n
-        X = tf.placeholder(tf.float32, ob_shape) #obs
+        X = tf.placeholder(tf.float32, ob_shape)
         with tf.variable_scope("model", reuse=reuse):
-            h1 = fc(X, 'fc1', nh=64, init_scale=np.sqrt(2))
-            h2 = fc(h1, 'fc2', nh=64, init_scale=np.sqrt(2))
-            pi = fc(h2, 'pi', nact, act=lambda x:x)
-            vf = fc(h2, 'v', 1, act=lambda x:x)
+            if USE_KERAS:
+                h1 = Dense(64, activation='relu', name='fc1')(X)
+                h2 = Dense(64, activation='relu', name='fc2')(h1)
+                pi = Dense(nact, name='pi')(h2)
+                vf = Dense(1, name='v')(h2)
+            else:
+                h1 = fc(X, 'fc1', nh=64, init_scale=np.sqrt(2))
+                h2 = fc(h1, 'fc2', nh=64, init_scale=np.sqrt(2))
+                pi = fc(h2, 'pi', nact, act=lambda x: x)
+                vf = fc(h2, 'v', 1, act=lambda x: x)
 
         v0 = vf[:, 0]
         a0 = sample(pi)
@@ -118,23 +125,31 @@ class FcPolicy(object):
         self.step = step
         self.value = value
 
-
+USE_KERAS = False
 class CnnPolicy(object):
 
     def __init__(self, sess, ob_space, ac_space, nenv, nsteps, nstack, reuse=False):
-        nbatch = nenv*nsteps
         nh, nw, nc = ob_space.shape
-        ob_shape = (nbatch, nh, nw, nc*nstack)
+        ob_shape = (None if USE_KERAS else nenv*nsteps, nh, nw, nc*nstack)
         nact = ac_space.n
         X = tf.placeholder(tf.uint8, ob_shape) #obs
         with tf.variable_scope("model", reuse=reuse):
-            h = conv(tf.cast(X, tf.float32)/255., 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2))
-            h2 = conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2))
-            h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
-            h3 = conv_to_fc(h3)
-            h4 = fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2))
-            pi = fc(h4, 'pi', nact, act=lambda x:x)
-            vf = fc(h4, 'v', 1, act=lambda x:x)
+            if USE_KERAS:
+                h = Conv2D(filters=32, kernel_size=8, strides=4, activation='relu', name='c1')(tf.cast(X, tf.float32)/255.)
+                h2 = Conv2D(64, 4, 2, activation='relu', name='c2')(h)
+                h3 = Conv2D(64, 3, 1, activation='relu', name='c3')(h2)
+                h3 = Flatten()(h3)
+                h5 = Dense(512, activation='relu', name='fc1')(h3)
+                pi = Dense(nact, name='pi')(h5)
+                vf = Dense(1, name='v')(h5)
+            else:
+                h = conv(tf.cast(X, tf.float32) / 255., 'c1', nf=32, rf=8, stride=4, init_scale=np.sqrt(2))
+                h2 = conv(h, 'c2', nf=64, rf=4, stride=2, init_scale=np.sqrt(2))
+                h3 = conv(h2, 'c3', nf=64, rf=3, stride=1, init_scale=np.sqrt(2))
+                h3 = conv_to_fc(h3)
+                h4 = fc(h3, 'fc1', nh=512, init_scale=np.sqrt(2))
+                pi = fc(h4, 'pi', nact, act=lambda x: x)
+                vf = fc(h4, 'v', 1, act=lambda x: x)
 
         v0 = vf[:, 0]
         a0 = sample(pi)
